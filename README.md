531be5e9dbae26cadba4082bf42b760b8298b326
zach <zach@zachpatrick.com>
2022-05-09 21:27:37 -0400

    streamline and give commands more consistent syntax; add ability to open code in browser

--- README.md
+++ README.md
@@ -20,38 +20,38 @@ nano .env
 
 ## Requirements
 
-You will need to have the [coder cli](https://github.com/coder/coder-cli) installed and configured. You will also need to create a coder api key by running the following command in your terminal:
+You will need to have the [coder cli](https://github.com/coder/coder-cli) installed and configured. We're also going to be using the Coder API to get workspaces, so you will also need to create a Coder API key by running the following command in your terminal:
 
 ```
 coder tokens create api_key
 ```
 
+Note: We are using the API to get the workspaces, rather than the coder cli, because the API is much faster.
+
 ## Usage
 
 Search for a workspace with the `coder` or `cdr` keywords. Select the workspace to start/stop it.
 
-You can pass the `-stop` flag to show all workspaces that are currently on and you can select one to it shutdown.
+There are also currently four tasks that you can use in addition to toggling the workspace.
+
+Some commands will open urls in your default browser. On the first line of the `Run Script` action you can set the browser to whatever browser you want to use. I have it set to `Firefox Developer Edition`.
+
+### Commands
+
+- `cdr workspace` will display the given workspaces with any commands that may be available. Certain commands will only be available if the workspace is running.
+
+- `cdr stop:workspace` will stop the given workspace
+
+- `cdr start:workspace` will start the given workspace
+
+- `cdr view:workspace` will open the frontend of given workspace in the browser. You can specify the base url with the `BASE_FRONTEND_URL` environment variable.
+
+- `cdr code:workspace` will open the code for the given workspace in the browser in your editor of choice. You can specify the editor with the `BASE_CODE_URL` environment variable.
+
+- `cdr stop:all` will stop all workspaces
 
 ```
-cdr -stop
-```
-
-You can pass the `-url` flag to show all workspaces that are currently on and you can select one to open the url in the browser.
-
-```
-cdr -url
-```
-
-You can pass the `-stopall` flag to stop all workspaces that are currently on.
-
-```
-cdr -stopall
-```
-
-In the `Run Script` action you can set the browser to whatever browser you want to use. I have it set to `Firefox Developer Edition`.
-
-```
-open -a "Firefox Developer Edition" $url
+browser="Firefox Developer Edition"
 ```
 
 ## References
--- src/main.js
+++ src/main.js
@@ -1,20 +1,114 @@
 import dotenv from 'dotenv';
 import alfy from 'alfy';
-
+import getWorkspaces from './getWorkspaces.js';
 dotenv.config();
 
-const url = `${process.env.ACCESS_URL}/${process.env.API_ROUTE}?users=${process.env.USER_ID}`
-const options = { headers: { 'Session-Token': `${process.env.API_KEY}` }, maxAge: 2000 };
-const workspaces = await alfy.fetch(url, options);
+(async () => {
+  const workspaces = await getWorkspaces();
+  const allowedActions = [`start`, `stop`, `view`, `code`];
 
-let items;
+  let items = [];
+  let action;
+  let workspace;
 
-items = alfy
-  .inputMatches(workspaces, 'name')
-  .map(element => ({
-    title: `${element.latest_stat.container_status === 'ON' ? 'Shut down' : 'Start'} ${element.name}`,
-    subtitle: `Status: ${element.latest_stat.container_status}`,
-		arg: [element.name, element.id, element.latest_stat.container_status]
-  }));
+  if (alfy.input.includes(`:`)) {
+    action = alfy.input.split(`:`)[0];
+    workspace = alfy.input.split(`:`)[1];
 
-alfy.output(items);
+    if (!allowedActions.includes(action)) {
+      return alfy.output({
+        title: `Action ${action} is not supported`,
+      });
+    }
+  } else {
+    workspace = alfy.input;
+  }
+
+  if (`stop:all`.includes(alfy.input)) {
+    items.push({
+      title: `Stop all`,
+      subtitle: `Shut down all workspaces`,
+      arg: [`Stop all`, `stopall`, null]
+    });
+  }
+
+  workspaces.map(el => {
+    if (el.name.includes(workspace)) {
+      if (action) {
+        if (el.latest_stat.container_status === `ON`) {
+          if (`view`.includes(action)) {
+            return items.push({
+              title: `${el.name}`,
+              subtitle: `https://${el.name}${process.env.BASE_FRONTEND_URL}`,
+              arg: [el.name, `view`, `https://${el.name}${process.env.BASE_FRONTEND_URL}`]
+            });
+          }
+
+          if (`code`.includes(action)) {
+            return items.push({
+              title: `${el.name}`,
+              subtitle: `Open ${el.name} in VS Code in the browser`,
+              arg: [el.name, `code`, `https://${process.env.BASE_CODE_URL}?workspaceId=${el.id}`]
+            });
+          }
+
+          if (`stop`.includes(action)) {
+            return items.push({
+              title: `${el.name}`,
+              subtitle: `Shut down ${el.name}`,
+              arg: [el.name, `stop`, null]
+            });
+          }
+        } else {
+          if (`start`.includes(action)) {
+            return items.push({
+              title: `${el.name}`,
+              subtitle: `Start ${el.name}`,
+              arg: [el.name, `start`, null]
+            });
+          }
+        }
+      } else {
+        items.push({
+          title: `${el.latest_stat.container_status === `ON` ? `Shut down` : `Start`} ${el.name}`,
+          subtitle: `Status: ${el.latest_stat.container_status}`,
+          arg: [el.name, el.id, el.latest_stat.container_status]
+        });
+
+        if (el.latest_stat.container_status === `ON`) {
+          items.push({
+            title: `Open the frontend of ${el.name} in the browser.`,
+            subtitle: `https://${el.name}${process.env.BASE_FRONTEND_URL}`,
+            arg: [el.name, `view`, `https://${el.name}${process.env.BASE_FRONTEND_URL}`]
+          });
+
+          items.push({
+            title: `Open ${el.name} in VS Code`,
+            subtitle: `https://${process.env.BASE_CODE_URL}?workspaceId=${el.id}.`,
+            arg: [el.name, `code`, `https://${process.env.BASE_CODE_URL}?workspaceId=${el.id}`]
+          });
+        }
+      }
+    }
+  });
+
+  if (action && !items.length) {
+    items.push({
+      title: `Ah snap!`,
+      subtitle: `Command is not available or no workspace was found with ${workspace} in it's name`,
+    });
+  } else if (!items.length && !allowedActions.includes(workspace)) {
+    items.push({
+      title: `Ah snap!`,
+      subtitle: `Could not find any workspace with ${workspace} in it's name`,
+    });
+  } else if (allowedActions.includes(workspace)) {
+    items.push({
+      title: `Keep going...`,
+      subtitle: `Add a colon to the end of the command to initialize an action. [${workspace}:workspace]`,
+    });
+  }
+
+  alfy.output(items);
+
+})()
